53c02449d76b1a07b23a0a1da3d364714056326d
victor franchetti <victorfranchetti@gmail.com>
2022-07-01 16:02:11 -0400

    Initial virtual env plus math

---
+++ equation.py
@@ -0,0 +1,8 @@
+def mult(x, y):
+     return x * y  
+
+
+def subt(x, y):
+    return x - y
+
+
---
+++ math.py
@@ -0,0 +1,9 @@
+import equation
+
+x = 5
+y = 8
+
+z = equation.mult(x, y)
+q = equation.subt(x, y)
+
+print(z + q)
