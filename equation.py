e25d3be81b8b033cf4e0912fd6e13584684dbc98
victor franchetti <victorfranchetti@gmail.com>
2022-07-01 17:11:44 -0400

    Addition branch added addition to equation and code to math

--- equation.py
+++ equation.py
@@ -1,8 +1,10 @@
 def mult(x, y):
-     return x * y  
+    return x * y
 
 
 def subt(x, y):
     return x - y
 
 
+def addi(x, y):
+    return x + y
--- math.py
+++ math.py
@@ -5,5 +5,6 @@ y = 8
 
 z = equation.mult(x, y)
 q = equation.subt(x, y)
+i = equation.addi(x, y)
 
 print(z + q)
